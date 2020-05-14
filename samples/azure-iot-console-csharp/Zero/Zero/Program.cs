890111fcd78657389bd13a1f1a2d42bcc36b8a98
Jesus Lopez <Jesus.Lopez2@ey.com>
2020-05-13 23:21:35 -0500

    Add console test app to trigget events in azure hub

---
+++ samples/azure-iot-console-csharp/Zero/Zero/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero
+{
+    class Program
+    {
+        private static ServiceClient s_serviceClient;
+        private readonly static string s_connectionString = "HostName=QiBase.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=1ggF1Ixns3UBKYUzULvOw7jLYwqhMGuB9lmkmkAY10o=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arre! \n");
+            s_serviceClient = ServiceClient.CreateFromConnectionString(s_connectionString);
+            InvokeMethod().GetAwaiter();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task InvokeMethod()
+        {
+            try
+            {
+                var commandMessage = new Message(Encoding.ASCII.GetBytes("Turn off"));
+                commandMessage.Properties.Add("ACTION", "OFF");
+                await s_serviceClient.SendAsync("qi-esp32", commandMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while sending message to device: \n{ex.Message}");
+                throw;
+            }
+        }
+    }
+}
