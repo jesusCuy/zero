1662cae592a6e4de487b4a15d176557f2c11ee3f
luis <ak_uit@hotmail.com>
2020-05-15 12:25:54 -0500

    add

--- web.ui/src/components/SetupForm/SetupForm.component.jsx
+++ web.ui/src/components/SetupForm/SetupForm.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import Paper from '@material-ui/core/Paper';
 import MtTextField from '@material-ui/core/TextField';
 import { FormControl, Select, MenuItem, InputLabel } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 import logo from "../../assets/LifeMask_bg.png"
+import { Context } from "../../context/index";
 
 import styles from "./SetupForm.module.css";
 import { withStyles } from '@material-ui/core/styles';
@@ -31,12 +33,28 @@ const TextField = withStyles({
     },
   })(MtTextField);
 
-  const handleChange = () => {
-
-  }
+  const locations = ['Hexaware Technologies México','Plaza Galerías','Soriana'];
+  const areas = ['Sector 1','Entrada','Salida de emergencia'];
+ 
 
 export default function TopNav () {
-  
+  const history = useHistory();
+  const user = React.useContext(Context);
+  const [values, setValues] = React.useState({
+      location: locations[0],
+      area: areas[0]
+    });
+
+  const handleOnClick = () => {
+    user.setAreaInformation(values);
+    history.push("/feedback");
+  };
+
+  const handleChange = event => {
+    const {value ,name} = event.target;
+    setValues({...values, [name]: value});
+  };
+
     return (
         <div className={styles["SetupFormWrapper"]}>
             <Paper  elevation={3} className={styles["SetupForm"]}> 
@@ -48,27 +66,29 @@ export default function TopNav () {
                 <FormControl variant="outlined" className={styles["formControlGroup"]}>
                     <InputLabel id="location-label">Ubicación</InputLabel>
                     <Select
+                      onChange={handleChange}
+                      name="location"
                       label="Ubicación"
                       id="outlined-location"
-                      value={ 1 }
-                      onChange={handleChange}
+                      value={ values.location }
                     >
-                      <MenuItem value={1}>Hexaware Technologies México</MenuItem>
-                      <MenuItem value={5}>Plaza Galerías</MenuItem>
-                      <MenuItem value={6}>Soriana</MenuItem>
+                      {locations.map(item =>{
+                        return (<MenuItem value={item}>{item}</MenuItem>)
+                      })}
                     </Select>
                   </FormControl>
                   <FormControl variant="outlined" className={styles["formControlGroup"]}>
                   <InputLabel id="area-label">Area</InputLabel>
                     <Select
+                      onChange={handleChange}
+                      name="area"
                       label="Sector"
                       id="outlined-area"
-                      value={ 1 }
-                      onChange={handleChange}
+                      value={ values.area }
                     >
-                      <MenuItem value={1}>Sector 1</MenuItem>
-                      <MenuItem value={2}>Entrada</MenuItem>
-                      <MenuItem value={3}>Salida de emergencia</MenuItem>
+                       {areas.map(item =>{
+                        return (<MenuItem value={item}>{item}</MenuItem>)
+                      })}
                     </Select>
                     </FormControl>
                     <FormControl className={styles["formControlGroup"]}>
@@ -76,7 +96,8 @@ export default function TopNav () {
                           variant="contained" 
                           size="large" 
                           color="primary"
-                          className={styles["accessButton"]}>
+                          className={styles["accessButton"]}
+                          onClick={handleOnClick}>
                           Acceder
                       </Button>
                     </FormControl>
